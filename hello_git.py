bbc9c0c665b483396e09bf2fa6a7a0ebcedbb807
zhoujing <jasonzhou@163.com>
2023-03-18 15:48:58 +0800

    Say hello to new guest Jerry.

--- hello_git.py
+++ hello_git.py
@@ -1,10 +1,12 @@
-name = 'Jason'
+name = 'Jack'
 guest = 'Tom'
+guest2 = 'Jerry'
 
 print("Hello Git world!")
 print("Hello everyone.")
 print(f'Hello {name}!')
 print(f'Hello {guest}!')
+print(f'Hello {guest2}!')
 
 
 
